b5ae7365d2da6fb7d2357794d9260da67a563071
Hana Mohan <unamashana@gmail.com>
2023-09-01 14:56:03 +0200

    awslocal - adds the http prefix to generate url from host

--- awslocal/config.go
+++ awslocal/config.go
@@ -15,7 +15,7 @@ type localResolver struct {
 }
 
 func (r localResolver) ResolveEndpoint(service string, region string, options ...interface{}) (aws.Endpoint, error) {
-	return aws.Endpoint{URL: (r.Host + r.Port)}, nil
+	return aws.Endpoint{URL: ("http://" + r.Host + r.Port)}, nil
 }
 
 func resolveWithLocalPort(host string, port string) aws.EndpointResolverWithOptions {
